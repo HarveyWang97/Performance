ec02b2cffd3711f5995d727dd9ba485d9fbcbf46
HarveyWang97 <461957024@qq.com>
2018-05-23 00:55:37 -0700

    update the collapse of chat-column

--- src/Components/Feedback_chat.js
+++ src/Components/Feedback_chat.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 
 const Feedback_chat = (props) => (
     <div>
-        
+        <div className="friend">
+            <img src={require("../Pictures/character.jpg")} />
+            <p>
+                <div className="chatName">Salman | Symantec</div>
+                <div className="chatAbbr">
+                    I really liked your wor...
+                </div>
+            </p>
+        </div>
+
+        <div className="friend">
+            <img src={require("../Pictures/character.jpg")} />
+            <p>
+                <div className="chatName">Salman | Symantec</div>
+                <div className="chatAbbr">I really liked your wor..</div>
+            </p>
+        </div>
     </div>
 );
+
+export default Feedback_chat;
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,45 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Feedback_chat from './Feedback_chat';
 //import './App.css';
 
-const Sidebar = (props) => (
-    <nav id="sidebar">
-                <div className="sidebar-header">
-                  <button type="button" className="btn btn-dark" id="search-btn">Searching Something....</button>
-                </div>
+export default class Sidebar extends React.Component {
 
-                <div className="list-group" id="sidebar_list-tab" role="tablist">
-                    <a className="list-group-item list-group-item-action active sidebar-item"  
-                        data-toggle="list" href="#" role="tab" aria-controls="home">
-                        My performance
-                    </a>
-                    <a className="list-group-item list-group-item-action sidebar-item"  data-toggle="list" href="#" role="tab" aria-controls="profile">
-                        Projects
-                    </a>
-                    <a className="list-group-item list-group-item-action sidebar-item"  data-toggle="list" href="#" role="tab" aria-controls="messages">
-                        1:1 Feedback
-                    </a>
-                </div>
+    state = {
+        chat_active:false
+    }
 
-                <div className="friend">
-			            <img src={require("../Pictures/character.jpg")} />
-			            <p>
-			                <div className="chatName">Salman | Symantec</div>
-                            <div className="chatAbbr">
-                                I really liked your wor...
-                            </div>
-			            </p>
-                </div>
-                
-                <div className="friend">
-			            <img src={require("../Pictures/character.jpg")} />
-			            <p>
-                            <div className="chatName">Salman | Symantec</div>
-                            <div className="chatAbbr">I really liked your wor..</div>
-			            </p>
-			    </div>
+    constructor(props){
+        super(props);
+        
+        this.toggleChat = this.toggleChat.bind(this);
+    }
 
-    </nav>
-);
+    toggleChat() {
+        const chat_active = !this.state.chat_active;
+        this.setState(() => ({chat_active:chat_active}));
+    }
+    
+    render(){
+        return (
+            <nav id="sidebar">
+                        <div className="sidebar-header">
+                          <button type="button" className="btn btn-dark" id="search-btn">Searching Something....</button>
+                        </div>
+        
+                        <div className="list-group" id="sidebar_list-tab" role="tablist">
+                            <a className="list-group-item list-group-item-action active sidebar-item"  
+                                data-toggle="list" href="#" role="tab" aria-controls="home">
+                                My performance
+                            </a>
+                            <a className="list-group-item list-group-item-action sidebar-item"  data-toggle="list" href="#" role="tab" aria-controls="profile">
+                                Projects
+                            </a>
+                            <a className="list-group-item list-group-item-action sidebar-item"  
+                                data-toggle="list" href="#" role="tab" aria-controls="messages"
+                                onClick={this.toggleChat}
+                            >
+                                1:1 Feedback
+                            </a>
+                        </div>
 
-export default Sidebar;
+                        <div className={this.state.chat_active?'show':'hidden'}>
+                            <Feedback_chat />
+                        </div>
+
+            </nav>
+        );
+    }
+}
